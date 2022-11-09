6d000873651f3688124c9abd48971d89d67b48a9
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-11-09 23:54:24 +0600

    authentication and signUp

--- src/Pages/SignUp/SignUP.js
+++ src/Pages/SignUp/SignUP.js
@@ -1,111 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import img from '../../images/SignUp/signUp.webp'
+import { AuthContext } from '../../Contexs/AuthProvider/AuthProvider';
 
 export const SignUp = () => {
+    const { createUser } = useContext(AuthContext)
+    const handleSignUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.error(error));
+
+    }
     return (
-        <div className="overflow-hidden bg-gray-900">
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-                <div className="flex flex-col items-center justify-between xl:flex-row">
-                    <div className="w-full max-w-xl mb-12 xl:pr-16 xl:mb-0 xl:w-7/12">
-                        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                            The quick, brown fox <br className="hidden md:block" />
-                            jumps over a{' '}
-                            <span className="text-teal-accent-400">lazy dog</span>
-                        </h2>
-                        <p className="max-w-xl mb-4 text-base text-gray-400 md:text-lg">
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                            accusantium doloremque laudan, totam rem aperiam, eaque ipsa quae.
-                        </p>
-                        <a
-                            href="/"
-                            aria-label=""
-                            className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700"
-                        >
-                            Learn more
-                            <svg
-                                className="inline-block w-3 ml-2"
-                                fill="currentColor"
-                                viewBox="0 0 12 12"
-                            >
-                                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-                            </svg>
-                        </a>
-                    </div>
-                    <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
-                        <div className="relative">
-                            <svg
-                                viewBox="0 0 52 24"
-                                fill="currentColor"
-                                className="absolute bottom-0 right-0 z-0 hidden w-32 -mb-8 -mr-20 text-teal-accent-400 lg:w-32 lg:-mr-16 sm:block"
-                            >
-                                <defs>
-                                    <pattern
-                                        id="766323e1-e594-4ffd-a688-e7275079d540"
-                                        x="0"
-                                        y="0"
-                                        width=".135"
-                                        height=".30"
-                                    >
-                                        <circle cx="1" cy="1" r=".7" />
-                                    </pattern>
-                                </defs>
-                                <rect
-                                    fill="url(#766323e1-e594-4ffd-a688-e7275079d540)"
-                                    width="52"
-                                    height="24"
-                                />
-                            </svg>
-                            <div className="relative bg-white rounded shadow-2xl p-7 sm:p-10">
-                                <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
-                                    Sign up for updates
-                                </h3>
-                                <form>
-                                    <div className="mb-1 sm:mb-2">
-                                        <label
-                                            htmlFor="name"
-                                            className="inline-block mb-1 font-medium"
-                                        >
-                                            Name
-                                        </label>
-                                        <input
-                                            placeholder="John Doe"
-                                            required
-                                            type="text"
-                                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                                            id="name"
-                                            name="name"
-                                        />
-                                    </div>
-                                    <div className="mb-1 sm:mb-2">
-                                        <label
-                                            htmlFor="email"
-                                            className="inline-block mb-1 font-medium"
-                                        >
-                                            E-mail
-                                        </label>
-                                        <input
-                                            placeholder="john.doe@example.org"
-                                            required
-                                            type="text"
-                                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                                            id="email"
-                                            name="email"
-                                        />
-                                    </div>
-                                    <div className="mt-4 mb-2 sm:mb-4">
-                                        <button
-                                            type="submit"
-                                            className="bg-orange-700 text-white w-full p-2 rounded-lg"
-                                        >
-                                            Subscribe
-                                        </button>
-                                    </div>
-                                    <p className="text-xs text-gray-600 sm:text-sm">
-                                        We respect your privacy. Unsubscribe at any time.
-                                    </p>
-                                </form>
-                            </div>
+        <div className="hero w-full my-20">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4' src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                    <h1 className="text-5xl text-center font-bold">SignUp</h1>
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="enter your name" className="input input-bordered" />
                         </div>
-                    </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className='text-center'>Have an account <Link className='text-orange-600 font-bold' to="/signin">Login</Link> </p>
                 </div>
             </div>
         </div>
